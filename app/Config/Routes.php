4ae8bd8ed8ea4d6877d1c97a387aa3186b5bad9d
adli <adlii.fiqrullah@gmail.com>
2023-11-02 21:20:37 +0700

    perbaikan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
+$routes->get('/home', 'Home::home');
 $routes->get('/login', 'Home::login');
